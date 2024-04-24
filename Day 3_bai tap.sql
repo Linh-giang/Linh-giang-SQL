25d57af5e9bfeec235c84c1c0d307c8bc3023626
Linh-giang <167959921+Linh-giang@users.noreply.github.com>
2024-04-25 00:35:02 +0700

    Create Day 3_bai tap.sql

---
+++ Day 3_bai tap.sql
@@ -0,0 +1,9 @@
+--baitap1
+select name from city
+where countrycode= 'USA' and  population > 120000
+--baitap2
+select * from city
+where countrycode = 'JPN'
+--baitap3
+select city, state from station
+--baitap4
